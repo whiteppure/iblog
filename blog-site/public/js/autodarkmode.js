38ec1a02286c0ea5311be4f25f79b0a704e5b20c
李济芝 <whitepure@gmail.com>
2024-03-15 14:01:40 +0800

    feat(post): 修复夜间模式

--- blog-site/public/js/autodarkmode.js
+++ blog-site/public/js/autodarkmode.js
@@ -1,24 +1,3 @@
 
-// 夜间模式使用
-(function (){
-    function darkModelOptions(){
-        return  {
-            right: '32px', // default: '32px'
-            bottom: 'unset', // default: '32px'
-            // left: '32px', // default: 'unset'
-            time: '0.3s', // default: '0.3s'
-            mixColor: '#f7f7f7', // default: '#fff'
-            backgroundColor: '#f7f7f7', // default: '#fff'
-            buttonColorDark: '#212121', // default: '#100f2c'
-            buttonColorLight: '#f7f7f7', // default: '#fff'
-            saveInCookies: false, // default: true,
-            autoMatchOsTheme: true // default: true
-        }
-    }
 
-    // 自动调用夜间模式
-    const hours = new Date().getHours();
-    if (hours >= 21 && hours <= 5){
-        new Darkmode(darkModelOptions()).toggle();
-    }
-})()
+
